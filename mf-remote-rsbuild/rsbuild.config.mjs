996032b6db558e5657aab50fffb3ba44a73462f1
vsp <vincent.spallek@varian.com>
2025-03-14 11:24:04 +0100

    Add public path

--- mf-remote-rsbuild/rsbuild.config.mjs
+++ mf-remote-rsbuild/rsbuild.config.mjs
@@ -1,7 +1,22 @@
-import { defineConfig } from '@rsbuild/core'
+import { defineConfig } from '@rslib/core'
 import { pluginModuleFederation } from '@module-federation/rsbuild-plugin'
 
 export default defineConfig({
+  output: {
+    publicPath: 'auto',
+  },
+  lib: [
+    {
+      format: 'esm',
+      output: {
+        publicPath: 'auto',
+      }
+    },
+  ],
+  server: {
+    port: 8767,
+    cors: true,
+  },
   plugins: [
     pluginModuleFederation({
       name: 'js_remote_rsbuild',
@@ -12,8 +27,4 @@ export default defineConfig({
       shared: ['markdown-it'],
     }),
   ],
-  server: {
-    port: 8767,
-    cors: true,
-  },
 })
